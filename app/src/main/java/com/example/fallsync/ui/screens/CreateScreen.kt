4cea8be036d045dac5852b71c5ec011596c6d4af
Uzziel <20243ds122@utez.edu.mx>
2025-12-09 12:54:33 -0600

    agregar layout inicial de CreateScreen con campos y botón

--- app/src/main/java/com/example/fallsync/ui/screens/CreateScreen.kt
+++ app/src/main/java/com/example/fallsync/ui/screens/CreateScreen.kt
@@ -1,8 +1,55 @@
 package com.example.fallsync.ui.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateScreen() {
 
+    var descripcion by remember { mutableStateOf("") }
+    var fecha by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Nuevo registro") })
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .padding(20.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+
+            Text("Descripción:")
+            OutlinedTextField(
+                value = descripcion,
+                onValueChange = { descripcion = it },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Text("Fecha:")
+            OutlinedTextField(
+                value = fecha,
+                onValueChange = { fecha = it },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = { /* sin lógica aún */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text("GUARDAR")
+            }
+        }
+    }
 }
