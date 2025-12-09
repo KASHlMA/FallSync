04855f257d36ef4e94ae384b8a174c72c57aee97
Uzziel <20243ds122@utez.edu.mx>
2025-12-09 13:03:16 -0600

    implementar POST a la API en CreateScreen

--- app/src/main/java/com/example/fallsync/ui/screens/CreateScreen.kt
+++ app/src/main/java/com/example/fallsync/ui/screens/CreateScreen.kt
@@ -1,8 +1,84 @@
 package com.example.fallsync.ui.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CreateScreen() {
+fun CreateScreen(navController: NavController) {
 
+    var descripcion by remember { mutableStateOf("") }
+    var fecha by remember { mutableStateOf("") }
+
+    val client = HttpClient()
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Nuevo registro") })
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .padding(20.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+
+            Text("Descripción:")
+            OutlinedTextField(
+                value = descripcion,
+                onValueChange = { descripcion = it },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Text("Fecha:")
+            OutlinedTextField(
+                value = fecha,
+                onValueChange = { fecha = it },
+                placeholder = { Text("YYYY-MM-DD") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = {
+                    scope.launch(Dispatchers.IO) {
+                        try {
+                            client.post("http:") {
+                                contentType(ContentType.Application.Json)
+                                setBody(
+                                    mapOf(
+                                        "descripcion" to descripcion,
+                                        "fecha" to fecha
+                                    )
+                                )
+                            }
+
+                            navController.popBackStack()
+
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text("GUARDAR")
+            }
+        }
+    }
 }
